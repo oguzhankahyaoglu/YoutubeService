b25d41f1cd2b050b96080ab14e3e4356b6aeb1cc
oguzhan.kahyaoglu <oguzhan.kahyaoglu@enerjisauretim.com>
2019-09-13 13:35:59 +0300

    fx

--- heroku-cli-build-push.bat
+++ heroku-cli-build-push.bat
@@ -2,8 +2,8 @@ REM - This file assumes that you have access to the application and that you hav
 REM : Setup your applications name below
 SET APP_NAME="ytdownloader-api"
 
-docker build -t %APP_NAME% .
-heroku container:push web -a %APP_NAME%
-heroku container:release web -a %APP_NAME%
+call docker build -t %APP_NAME% .
+call heroku container:push web -a %APP_NAME%
+call heroku container:release web -a %APP_NAME%
 
 PAUSE
